c9d95e4bdffd96aed10a54886676e0e772cb8af6
sumanf <flavien.suman@etu.univ-grenoble-alpes.fr>
2023-02-01 10:34:52 +0100

    ajout fabricants

--- src/main/java/fr/uga/miage/m1/repository/FabricantsRepository.java
+++ src/main/java/fr/uga/miage/m1/repository/FabricantsRepository.java
@@ -2,12 +2,13 @@ package fr.uga.miage.m1.repository;
 
 import fr.uga.miage.m1.entity.Fabricant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface FabricantsRepository extends JpaRepository<Fabricant, Long> {
+public interface FabricantsRepository extends JpaRepository<Fabricant, Long>, JpaSpecificationExecutor<Fabricant> {
     Boolean existsByLibelle(String libelle); //Checks if there are any records by name
 
     Optional<Fabricant> getByLibelle(String libelle);
